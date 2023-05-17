72b5eccccf40fbe8176a4b3744d1edff2669c470
Michał Murawski <m.a.murawski@wp.pl>
2023-05-17 11:56:40 +0200

    background SharredLayout

--- src/components/utils/IconSVG/IconSVG.jsx
+++ src/components/utils/IconSVG/IconSVG.jsx
@@ -1,15 +1,19 @@
-// import { ReactSVG } from 'react-svg';
+import { ReactSVG } from 'react-svg';
 import icons from '../../../images/icons.svg';
-import scss from './IconSVG.module.css';
+import scss from './IconSVG.module.scss';
 
-export const IconSVG = ({ name, width, height, fill }) => {
+export const IconSVG = ({ name, width, height, fill, transform }) => {
   return (
     <svg
       className={scss.icons}
-      viewBox="0 0 30 30"
-      style={{ width, height, fill }}
+      viewBox={'0 0 ' + width + ' ' + height}
+      style={{ width, height, fill: `var(${fill})` }}
     >
-      <use xlinkHref={icons + '#icon-' + name} width="30" height="30"></use>
+      <use
+        xlinkHref={icons + '#icon-' + name}
+        width={width}
+        height={height}
+      ></use>
     </svg>
   );
 };
--- src/layouts/SharedLayout/SharedLayout.jsx
+++ src/layouts/SharedLayout/SharedLayout.jsx
@@ -1,18 +1,28 @@
 import { Suspense } from 'react';
-// import scss from './SharedLayout.module.scss';
+import scss from './SharedLayout.module.scss';
 import { Outlet } from 'react-router-dom';
 import { IconSVG } from '../../components/utils/IconSVG/IconSVG';
 
 export const SharedLayout = () => {
   return (
     <div>
-      <div>
-        <IconSVG
-          name={'ellipse'}
-          width={'100px'}
-          height={'100px'}
-          fill={'blue'}
-        />
+      <div className={scss.bg}>
+        <div className={scss['bg-ellipse1']}>
+          <IconSVG
+            name={'ellipse'}
+            width={'618px'}
+            height={'547px'}
+            fill={'--icon-elipse1'}
+          />
+        </div>
+        <div className={scss['bg-ellipse2']}>
+          <IconSVG
+            name={'ellipse'}
+            width={'618px'}
+            height={'547px'}
+            fill={'--icon-elipse2'}
+          />
+        </div>
       </div>
       <Suspense>
         <Outlet />
