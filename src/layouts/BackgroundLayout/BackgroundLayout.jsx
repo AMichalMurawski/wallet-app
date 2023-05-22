8987f15db2f32833a3f59533d7da497bc9d08bcd
Michał Murawski <m.a.murawski@wp.pl>
2023-05-22 17:33:20 +0200

    BackgroundLayout - Box

--- src/layouts/BackgroundLayout/BackgroundLayout.jsx
+++ src/layouts/BackgroundLayout/BackgroundLayout.jsx
@@ -2,28 +2,29 @@ import { Suspense } from 'react';
 import scss from './BackgroundLayout.module.scss';
 import { Outlet } from 'react-router-dom';
 import { IconSVG } from '../../components/icons/IconSVG/IconSVG';
+import { Box } from '@mui/material';
 
 export const BackgroundLayout = () => {
   return (
     <div>
-      <div className={scss.bg}>
-        <div className={scss['bg-ellipse1']}>
+      <Box fixed minWidth="100vw" minHeight="100%" className={scss.bg}>
+        <Box className={scss['bg-ellipse1']}>
           <IconSVG
             name={'ellipse'}
             width={'618px'}
             height={'547px'}
             fill={'--icon-elipse1'}
           />
-        </div>
-        <div className={scss['bg-ellipse2']}>
+        </Box>
+        <Box className={scss['bg-ellipse2']}>
           <IconSVG
             name={'ellipse'}
             width={'618px'}
             height={'547px'}
             fill={'--icon-elipse2'}
           />
-        </div>
-      </div>
+        </Box>
+      </Box>
       <Suspense>
         <Outlet />
       </Suspense>
