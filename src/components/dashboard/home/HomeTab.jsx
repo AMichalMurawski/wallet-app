0af80d87f0beafb379ab567b14a8505ba5fed145
Michał Murawski <m.a.murawski@wp.pl>
2023-06-12 13:08:17 +0200

    Balance in mobile HomeTab

--- src/components/dashboard/home/HomeTab.jsx
+++ src/components/dashboard/home/HomeTab.jsx
@@ -1,9 +1,23 @@
 import { Box, Typography } from '@mui/material';
+import { useMediaMui } from '../../../hooks';
+import { Balance } from '../Balance';
 
 const HomeTab = () => {
+  const mediaMui = useMediaMui();
   return (
     <Box>
-      <Typography color="text.main">Home Component</Typography>
+      {!mediaMui.tablet ? (
+        <Box
+          sx={{
+            width: 1,
+            display: 'flex',
+            justifyContent: 'center',
+            padding: '12px',
+          }}
+        >
+          <Balance />
+        </Box>
+      ) : null}
     </Box>
   );
 };
